1fb5266c09616585fb8b0c7fb13c2409955f7e77
huchao <huchao@zafh.com.cn>
2018-12-09 19:20:25 +0800

    加注释

--- rate-limiter-server/src/main/java/cn/caijiajia/ratelimiter/server/form/RateLimiterForm.java
+++ rate-limiter-server/src/main/java/cn/caijiajia/ratelimiter/server/form/RateLimiterForm.java
@@ -25,12 +25,24 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RateLimiterForm {
+    /**
+     * 令牌桶唯一标识
+     */
     @NotBlank
     private String name;
+    /**
+     * 能够使用令牌桶的应用列表
+     */
     @NotBlank
     private String context;
+    /**
+     * 令牌桶容量
+     */
     @NotNull
     private Integer maxPermits;
+    /**
+     * 向令牌桶中添加令牌的速率
+     */
     @NotNull
     private Integer rate;
 }
